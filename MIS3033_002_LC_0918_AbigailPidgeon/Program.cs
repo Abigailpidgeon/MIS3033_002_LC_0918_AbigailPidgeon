369c40a42de348ca0e7ab5fa15ac556ea7ce21ff
Abby Pidgeon <Abigail.L.Pidgeon-1@ou.edu>
2023-09-18 11:44:58 -0500

    finished lc

--- MIS3033_002_LC_0918_AbigailPidgeon/Program.cs
+++ MIS3033_002_LC_0918_AbigailPidgeon/Program.cs
@@ -3,17 +3,87 @@
 // Abigail Pidgeon 113515288
 
 using a;
+using System.Text.Json;
 
 Console.WriteLine("Create grades database");
 
-StudentGradeDB db;
-db = new StudentGradeDB();//create a database connection
+StudentGradeDB db; // complex
+db = new StudentGradeDB();//create database connection
 
-double r1 = db.StugradesTbl.Average(s => s.grade);
-Console.WriteLine( r1);
+var opt = new JsonSerializerOptions();
+opt.WriteIndented = true;
 
-double r2 = db.StugradesTbl.Max(s => s.grade);
-Console.WriteLine(r2);
+string filepath = "data.json";
+string jsonstr;
 
-double r3 = db.StugradesTbl.Min(s => s.grade);
-Console.WriteLine(r3);
+bool checkgrade(StudentGrade x)
+{
+    if (x.grade >= 80 && x.grade <= 90)
+    {
+        return true;
+    }
+    else 
+    { 
+        return false; 
+    }    
+    
+} 
+
+//lamda expression
+{
+    //var r = db.StugradesTbl;//collection table, ctrl d
+    // var r = db.StugradesTbl.Where(x=>x.Age==20);//collection table 
+    //  var r = db.StugradesTbl.Where(checkgrade);//collection table 
+
+    // where() in list, collection, table, list
+    // where() work on collection,list,table
+    //var r = db.StugradesTbl.Where((s) => s.grade >= 80 && s.grade <= 90);
+    // var r = db.StugradesTbl.Where((s) => s.grade >= 80 && s.grade <= 90 && s.Age == 20);
+    //var r = db.StugradesTbl.Where((s) => s.grade >= 80 && s.grade <= 90).Where(S => S.Age == 18);
+    //var r = db.StugradesTbl.Where(x =>x.grade>90).Where(x=>x.grade<95);
+    //var r = db.StugradesTbl.Where(x =>x.grade>90).FirstOrDefault();
+    var r = db.StugradesTbl.FirstOrDefault();
+
+    r.Age = 100;
+
+    db.StugradesTbl.Remove(r);
+
+//db.SaveChanges();
+//    {
+//        if (s.grade >= 80 &&  s.grade <= 90)
+//        {
+//            return true;
+//        } 
+//        else
+//        {
+//            return false;
+//        }
+//    });//collection table 
+
+    jsonstr = JsonSerializer.Serialize(r,opt);
+    File.WriteAllText(filepath,jsonstr);
+
+}
+
+//complex
+{
+    StudentGrade s1;
+    StudentGrade s2;
+
+    s1 = new StudentGrade();
+    s2 = new StudentGrade();
+    s1 = s2;// 
+}
+
+
+//StudentGradeDB db;
+//db = new StudentGradeDB();//create a database connection
+
+//double r1 = db.StugradesTbl.Average(s => s.grade);
+//Console.WriteLine( r1);
+
+//double r2 = db.StugradesTbl.Max(s => s.grade);
+//Console.WriteLine(r2);
+
+//double r3 = db.StugradesTbl.Min(s => s.grade);
+//Console.WriteLine(r3);
